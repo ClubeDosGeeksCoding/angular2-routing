323c877dd64ec36c07e0c3c63b2defcf99397d51
JayrAlencar <jayralencarpereira@gmail.com>
2016-05-08 18:52:01 -0300

    again

--- app/main.ts
+++ app/main.ts
@@ -1,3 +1,4 @@
 import { bootstrap }    from 'angular2/platform/browser';
+import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from 'angular2/router';
 import { AppComponent } from './app.component';
 bootstrap(AppComponent);
