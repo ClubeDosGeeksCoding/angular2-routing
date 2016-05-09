9ea2a8184f2b4454372ce19d46b07736a60fa8ae
Jayr Alencar <jayralencarpereira@gmail.com>
2016-05-09 13:55:20 -0300

    Ok

--- app/main.ts
+++ app/main.ts
@@ -1,7 +1,4 @@
 import { bootstrap }    from 'angular2/platform/browser';
 import { AppComponent } from './app.component';
-import 'rxjs/Rx';
-import { HTTP_PROVIDERS } from 'angular2/http';
 
-
-bootstrap(AppComponent, [HTTP_PROVIDERS]);
+bootstrap(AppComponent);
